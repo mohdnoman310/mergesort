371e92bbb927c60c8d7969b9616d8b4c2daa7590
mohdnoman310 <153140862+mohdnoman310@users.noreply.github.com>
2023-12-06 18:49:31 -0500

    Add files via upload

---
+++ merge code.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        // Same as (left+right)/2, but avoids overflow for large left and right
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+
+    std::cout << "Original Array:";
+    for (int num : arr) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
+
+    merge_sort(arr, 0, arr.size() - 1);
+
+    std::cout << "Sorted Array:";
+    for (int num : arr) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
